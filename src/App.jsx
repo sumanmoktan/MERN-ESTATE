d110bbd5d1e24e4c7e88690c2f0fff10fa0a52e8
sumanmoktan <sumantamang662@gmail.com>
2023-12-06 14:24:22 +0545

    changes in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
-import SignOut from "./pages/SignOut"
 import Header from "./components/Header"
+import SignUp from "./pages/SignUp"
 
 function App() {
   
@@ -17,7 +17,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/signin" element={<SignIn/>}/>
-        <Route path="/signout" element={<SignOut/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
       </Routes>
     </BrowserRouter>
     
