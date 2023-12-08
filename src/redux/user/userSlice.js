cc490cac8740c79d853404f02b22b49fcfeabc6a
sumanmoktan <sumantamang662@gmail.com>
2023-12-08 12:47:54 +0545

    changes in redux

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -34,30 +34,30 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
-    // deleteUserStart: (state) => {
-    //   state.loading = true;
-    // },
-    // deleteUserSuccess: (state) => {
-    //   state.currentUser = null;
-    //   state.loading = false;
-    //   state.error = null;
-    // },
-    // deleteUserFailure: (state, action) => {
-    //   state.error = action.payload;
-    //   state.loading = false;
-    // },
-    // signOutUserStart: (state) => {
-    //   state.loading = true;
-    // },
-    // signOutUserSuccess: (state) => {
-    //   state.currentUser = null;
-    //   state.loading = false;
-    //   state.error = null;
-    // },
-    // signOutUserFailure: (state, action) => {
-    //   state.error = action.payload;
-    //   state.loading = false;
-    // },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -68,12 +68,12 @@ export const {
   updateUserFailure,
   updateUserSuccess,
   updateUserStart,
-//   deleteUserFailure,
-//   deleteUserSuccess,
-//   deleteUserStart,
-//   signOutUserFailure,
-//   signOutUserSuccess,
-//   signOutUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;
